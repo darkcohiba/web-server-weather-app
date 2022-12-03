2d92b79725c8fb7f14b798dee45ed921c131f0c3
Sam <samuel.waters@gmail.com>
2022-12-03 13:34:24 -0700

    feat:added hbs template for help page and set up express route

--- src/app.js
+++ src/app.js
@@ -11,12 +11,27 @@ const directoryPath = path.join(__dirname, '../public');
 app.set('view engine', 'hbs');
 app.use(express.static(directoryPath));
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index',{
         title: 'Weather',
         name: 'Andrew Mead'
     });
 });
+
+app.get("/about", (req, res) => {
+    res.render('about',{
+        title: 'About Me',
+        name: 'Andrew Mead'
+    } )
+}) 
+
+app.get("/help", (req, res) => {
+    res.render('help',{
+        title: 'Help',
+        message: 'This is a help message',
+        
+        } )
+})
 
 app.get("/weather", (req, res) => {
     res.send({"Weather Page": "Sunny","Temperature": "25"});
