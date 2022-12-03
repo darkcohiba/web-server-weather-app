2e4ba24769c728e7bb9e9f61606490ec14dc72be
Sam <samuel.waters@gmail.com>
2022-12-03 13:18:58 -0700

    feat:added handlebar and views

--- src/app.js
+++ src/app.js
@@ -8,7 +8,15 @@ console.log(path.join(__dirname, '../public'));
 const directoryPath = path.join(__dirname, '../public');
 
 
+app.set('view engine', 'hbs');
 app.use(express.static(directoryPath));
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather',
+        name: 'Andrew Mead'
+    });
+});
 
 app.get("/weather", (req, res) => {
     res.send({"Weather Page": "Sunny","Temperature": "25"});
