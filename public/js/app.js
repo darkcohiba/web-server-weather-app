b83229cbc5538571352849d7fed930ffb1861d21
Sam <samuel.waters@gmail.com>
2023-01-10 12:23:31 -0700

    feat:added ability to search for weather by location on main page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
+//selectors
+const weatherForm = document.querySelector('form')
+const searchTerm = document.querySelector('input')
+
 console.log("Client side javascript is loading");
+
+
+// fetch('https://puzzle.mead.io/puzzle')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+    
+
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const location = searchTerm.value
+    console.log(location)
+
+    fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(response => response.json())
+    .then(data => {
+        if (data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forcast)
+        }
+    })
+})
