485ef7ba86978067d6a2eb2aee4a79a3eec30c2a
Sam <samuel.waters@gmail.com>
2023-01-11 11:24:32 -0700

    feat:finished weather app minus deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,27 @@
 //selectors
 const weatherForm = document.querySelector('form')
 const searchTerm = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 console.log("Client side javascript is loading");
-
-
-// fetch('https://puzzle.mead.io/puzzle')
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//     })
-    
-
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     const location = searchTerm.value
-    console.log(location)
+
+    messageOne.textContent = 'Loading weather'
+    messageTwo.textContent= ""
 
     fetch(`http://localhost:3000/weather?address=${location}`)
     .then(response => response.json())
     .then(data => {
         if (data.error){
-            console.log(data.error)
+            messageOne.textContent= `${data.error}`
+            messageTwo.textContent= ""
         }else{
-            console.log(data.location)
-            console.log(data.forcast)
+            messageOne.textContent = data.location
+            messageTwo.textContent= `${data.forcast}`
         }
     })
 })
